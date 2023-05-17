d4c1304a1661dce0fa6e8bf8848edb6c021add64
tristanOry <t.ory@laposte.net>
2023-05-17 14:26:17 +0200

    Ajout scripts d'installation pour windows (install.cmd) et linux/macOs (install.sh)

---
+++ install.cmd
@@ -0,0 +1,9 @@
+@echo off
+composer update
+composer install
+npm install
+npm run build
+php bin/console doctrine:database:create
+php bin/console doctrine:migrations:migrate
+php bin/console doctrine:query:sql < type triggers.sql
+php bin/console doctrine:fixtures:load
