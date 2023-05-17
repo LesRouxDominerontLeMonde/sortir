f1539bf56c9fba8b83fe62416ebc22723f426ff3
MetaMayaHeol <lageatromuald@gmail.com>
2023-05-17 14:07:59 +0200

    Modification confirmation mdp

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -31,22 +32,22 @@ class RegistrationFormType extends AbstractType
                 'choice_label' => 'nom',
                 'placeholder' => 'Choisissez votre campus',
             ])
-            ->add('password', PasswordType::class)
-            ->add('plainPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'constraints' => [
-                    new NotBlank([
-                        'message' => 'Please enter a password',
-                    ]),
-                    new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
-                        // max length allowed by Symfony for security reasons
-                        'max' => 4096,
-                    ]),
+            ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'Les mots de passes doivent être identiques.',
+            'options' => ['attr' => ['autocomplete' => 'new-password']],
+            'required' => true,
+            'first_options' => ['label' => 'Password'],
+            'second_options' => ['label' => 'Repeat Password'],
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Entrer un mot de passe',
+                ]),
+                new Length([
+                    'min' => 6,
+                    'minMessage' => 'Votre mot de passe doit contenir au minimum {{ limit }} caractères',
+                    'max' => 4096,
+                ]),
                 ],
             ])
         ;
