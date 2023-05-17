b61a4ddfac42040c4b768767547ef911f15d44c5
jamelboucif <jamelboucif@hotmail.fr>
2023-05-17 15:12:05 +0200

    creation controller profil et profil twig base

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -13,7 +13,7 @@ class ProfilController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('profil/index.html.twig', [
+        return $this->render('profil.html.twig', [
             'controller_name' => 'ProfilController',
         ]);
     }
