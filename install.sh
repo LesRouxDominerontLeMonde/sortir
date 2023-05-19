8bf1055a5d7e498227ae330dca0670a16bf9c203
tristanOry <t.ory@laposte.net>
2023-05-19 09:07:16 +0200

    Ajout scripts d'installation pour windows (install.cmd) et linux/macOs (install.sh)

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ npm install
 npm run build
 php bin/console doctrine:database:create
 php bin/console doctrine:migrations:migrate
-php bin/console doctrine:query:sql < type triggers.sql
 php bin/console doctrine:fixtures:load
+echo "Pensez bien à faire exécuter le script triggers.sql par mysql"
+pause
