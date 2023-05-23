67f8d77a38099a67bbc9b9aacd9cda3b5bbe57a3
jamelboucif <jamelboucif@hotmail.fr>
2023-05-23 10:01:52 +0200

    ajouter photo profil et modifier + twig affichage profil et photo

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Security;
 
 class ProfilController extends AbstractController
 {
+    protected $projectDir;
+
+    public function __construct(KernelInterface $kernel, UserRepository $userRepository)
+    {
+        $this->projectDir = $kernel->getProjectDir();
+    }
     /**
      * @Route("/profil", name="app_profil")
      */
-    public function index(): Response
+    public function index(Security $security): Response
     {
+        $user = $security->getUser();
+        $dir = $this->projectDir.'/public/image/'.$user->getId();
+        $filesystem = new Filesystem();
+        $filesystem->mkdir($dir);
+
         return $this->render('profil/profil.html.twig', [
-            'controller_name' => 'ProfilController',
+            'user' => $user,
         ]);
     }
 }
