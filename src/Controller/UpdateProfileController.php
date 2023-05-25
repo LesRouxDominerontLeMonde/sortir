796a758344e6528a40d8c4509f44d27ad2662c0a
tristanOry <t.ory@laposte.net>
2023-05-25 11:04:06 +0200

    Mise en page du formulaire de création d'utilisateur. Début du controleur de mise à jour du profil

---
+++ src/Controller/UpdateProfileController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Photo;
+use App\Form\UpdateProfileFormType;
+use App\Security\AppAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
+
+class UpdateProfileController extends AbstractController
+{
+    /**
+     * @Route("/update/profil", name="app_update_profil")
+     */
+    public function update(Request $request,
+                           UserPasswordHasherInterface $userPasswordHasher,
+                           UserAuthenticatorInterface $UserAuthenticator,
+                           AppAuthenticator $appAuthenticator,
+                           EntityManagerInterface $entityManager): Response
+    {
+        if(!$this->getUser()){
+            throw new AccessDeniedHttpException('Accès refusé.');
+        }
+
+        $user = $this->getUser();
+        $oldPassword = $user->getPassword();
+        $form = $this->createForm(UpdateProfileFormType::class, $user, []);
+
+        $form->handleRequest();
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $user->setActif(true);
+            if($form->get('password')->getData() != '') {
+                $user->setPassword($userPasswordHasher->hashPassword($form->get('password')->getData()));
+            } else {
+                $user->setPassword($oldPassword);
+            }
+            $photo = $form->get('photo')->getData();
+            // Si il y a un fichier dans les données du formulaire et qu'il n'existe pas
+            $fs = new Filesystem();
+            if ($photo && !$fs->exists($this->getParameter('profile_image_directory', $photo->getClientOriginalName())));
+            {
+                // On vérifie le type
+                $mime = $photo->getMimeType();
+                if($mime === 'image/jpeg' || $mime === 'image/png'){
+                    // Si le type es ok, on donne un nom de fichier unique
+                    $filename = uniqid().'.'.$photo->guessExtension();
+                    // On l'enregistre dans le dossier enregistré dans config/services.yaml
+                    $photo->move($this->getParameter('profile_image_directory'), $filename);
+                    // On crée l'entité pour enregistrer le fichier dans la base de données
+                    $entityManager->persist($user);
+                    $photoEntity = new Photo();
+                    $photoEntity->setNomFichier($filename)
+                        ->setUtilisateur($user)
+                        ->setActive(true);
+                    $entityManager->persist($photoEntity);
+                    $entityManager->flush();
+                    // Puis on lie la photo au profil utilisateur
+                    $user->addPhoto($photoEntity);
+                }
+            }
+        }
+
+        return $this->render('update_profile/index.html.twig', [
+            'controller_name' => 'UpdateProfileController',
+        ]);
+    }
+}
---
+++ src/Form/UpdateProfileFormType.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\User;
+use App\Repository\CampusRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class UpdateProfileFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $user = $options['data'];
+        $builder
+        ->add('pseudo', TextType::class, [
+            'data' => $user->getPseudo()
+    ])
+        ->add('firstname', TextType::class, [
+            'data' => $user->getFirstname(),
+            'label' => 'prénom',
+        ])
+        ->add('name', TextType::class, [
+            'data' => $user->getName(),
+            'label' => 'nom',
+        ])
+        ->add('phoneNumber', TextType::class, [
+            'data' => 'O'.$user->getPhoneNumber(),
+            'label' => 'téléphone',
+        ])
+        ->add('email', EmailType::class)
+        ->add('campus', EntityType::class, [
+            'class' => Campus::class,
+            'choice_label' => 'campus',
+            'placeholder' => 'Choisissez votre campus',
+            'choice_value' => 'id',
+            'choice_attr' => function ($choice, $key, $value) use ($user) {
+                // Définir l'attribut 'selected' pour la valeur par défaut
+                if ($user->getCampus() && $user->getCampus()->getId() === $value) {
+                    return ['selected' => 'selected'];
+                }
+
+                return [];
+            },
+        ])
+        ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'Les mots de passes doivent être identiques.',
+            'options' => ['attr' => ['autocomplete' => 'new-password']],
+            'required' => false,
+            'first_options' => ['label' => 'Password'],
+            'second_options' => ['label' => 'Repeat Password'],
+            'constraints' => [
+                new NotBlank([
+                    'message' => 'Entrer un mot de passe',
+                ]),
+                new Length([
+                    'min' => 6,
+                    'minMessage' => 'Votre mot de passe doit contenir au minimum {{ limit }} caractères',
+                    'max' => 4096,
+                ]),
+            ],
+        ])
+        ->add('photo', FileType::class, [
+            'label' => 'Image de profil',
+            'required' => false,
+            'mapped' => false,
+        ])
+    ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
