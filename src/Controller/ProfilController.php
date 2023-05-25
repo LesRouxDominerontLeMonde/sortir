82f4b240d3eced4ade68c0599faf53240fc91996
MetaMayaHeol <lageatromuald@gmail.com>
2023-05-25 16:04:03 +0200

    Affichage profil d'un participant d'une sortie

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -28,6 +30,24 @@ class ProfilController extends AbstractController
 
         return $this->render('profil/profil.html.twig', [
             'user' => $user,
+            'photo' => $user->getCurrentPhotoName(),
+            'photo_path' => $this->getParameter('profile_image_directory') .'/',
+        ]);
+    }
+
+    /**
+     * @Route("profil/{id}", name="app_profil_affiche", requirements={"id"="\d+"})
+     */
+    public function afficheProfilById(Request $request, ManagerRegistry $doctrine, UserRepository $userRepository)
+    {
+        $id = $request->get('id');
+        $user = $userRepository->findOneBy(['id' => $id]);
+        if (!$user) {
+            throw $this->createNotFoundException('Le participant demandé n\'existe pas.');
+        }
+
+        return $this->render('profil/afficheParticipant.html.twig', [
+            'participant' => $user,
             'photo' => $user->getCurrentPhotoName(),
             'photo_path' => $this->getParameter('profile_image_directory') .'/',
         ]);
